6bb679d586e4f6d46a005d6b4deb2f774a7ad291
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2024-01-28 18:54:31 +0700

    update

---
+++ js/particle.js
@@ -0,0 +1,41 @@
+class Particle {
+  constructor() {
+    this.position = new Point();
+    this.velocity = new Point();
+    this.acceleration = new Point();
+    this.age = 0;
+  }
+
+  initialize(x, y, dx, dy) {
+    this.position.x = x;
+    this.position.y = y;
+    this.velocity.x = dx;
+    this.velocity.y = dy;
+    this.acceleration.x = dx * settings.particles.effect;
+    this.acceleration.y = dy * settings.particles.effect;
+    this.age = 0;
+  }
+
+  update(deltaTime) {
+    this.position.x += this.velocity.x * deltaTime;
+    this.position.y += this.velocity.y * deltaTime;
+    this.velocity.x += this.acceleration.x * deltaTime;
+    this.velocity.y += this.acceleration.y * deltaTime;
+    this.age += deltaTime;
+  }
+
+  draw(context, image) {
+    function ease(t) {
+      return --t * t * t + 1;
+    }
+    const size = image.width * ease(this.age / settings.particles.duration);
+    context.globalAlpha = 1 - this.age / settings.particles.duration;
+    context.drawImage(
+      image,
+      this.position.x - size / 2,
+      this.position.y - size / 2,
+      size,
+      size
+    );
+  }
+}
---
+++ js/settings.js
@@ -0,0 +1,10 @@
+var settings = {
+  particles: {
+    length: 500,
+    duration: 2,
+    velocity: 100,
+    effect: -0.75,
+    size: 30,
+  },
+  dateStart: "08/31/2023",
+};
